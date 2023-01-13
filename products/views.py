eefed6bd727ed5e336c67886960db45ce895484f
LucaNoah <luca.gfeller2@gmail.com>
2023-01-13 18:43:21 +0000

    in base.htm add attributes to form input from the search field, in products app all_products view add functionality to search all products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,6 +9,16 @@ def all_products(request):
     """ A view to display all products as search queries and sorting"""
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(request, 'You have not entered any search terms!')
+                return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
