b368ab5985870ef0a3c5beef707e648978ed6fe9
LucaNoah <luca.gfeller2@gmail.com>
2023-01-17 14:59:40 +0000

    in base.html add urls to navbar links, in products app rename all_products view to list_products and add functionality to display products by category

--- products/views.py
+++ products/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
-def all_products(request):
-    """ A view to display all products as search queries and sorting"""
+def list_products(request):
+    """ A view to list either all products, specifically categorized products, or products requested by search query. """
 
     products = Product.objects.all()
+    search_query = None
+    categories_queried = None
 
     if request.GET:
-        if 'query' in request.GET:
-            query = request.GET['query']
-            if not query:
-                messages.error(request, 'You have not entered any search terms!')
-                return redirect(reverse('products'))
+        if 'category' in request.GET:
+            categories_queried = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories_queried)
+            categories_queried = Category.objects.filter(name__in=categories_queried)
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+        if 'query' in request.GET:
+            search_query = request.GET['query']
+            if not search_query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': search_query,
+        'current_categories': categories_queried,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
