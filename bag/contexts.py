62c996c7661d58991209f881e7689af7aff4aea4
LucaNoah <luca.gfeller2@gmail.com>
2023-01-27 13:31:13 +0000

    in bag app create context.py & bag_content context processor, in settings.py create 2 new variables to calculate delivery costs & add bag_content to template context processors

---
+++ bag/contexts.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_content(request):
+
+    bag_items = []
+    total = 0
+    bag_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery_cost = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_difference = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery_cost = 0
+        free_delivery_difference = 0
+
+    grand_total = delivery_cost + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'bag_count': bag_count,
+        'delivery_cost': delivery_cost,
+        'free_delivery_difference': free_delivery_difference,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
--- gfeller_herbs/settings.py
+++ gfeller_herbs/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_content',
             ],
         },
     },
@@ -154,3 +155,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 7
