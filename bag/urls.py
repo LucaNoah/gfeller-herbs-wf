a7c40d3f00d92edb2f94f18c26c4866ed25ab643
LucaNoah <luca.gfeller2@gmail.com>
2023-01-27 14:28:13 +0000

    in bag app create view add_item to add functionality to add an item to the shopping bag

--- bag/urls.py
+++ bag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='shopping_bag'),
+    path('add-item/<product_id>', views.add_item, name='add_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ def view_bag(request):
     """ A view to return the shopping bag content """
     
     return render(request, 'bag/shopping_bag.html')
+
+def add_item(request, product_id):
+    """ Add a certain number to the shopping bag """
+
+    quanity = int(request.POST.get('quanity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quanity
+    else:
+        bag[product_id] = quanity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
