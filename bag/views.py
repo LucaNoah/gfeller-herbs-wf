28b82c40e0aedd9f49351ac543790e965a4b7dfc
LucaNoah <luca.gfeller2@gmail.com>
2023-01-31 14:00:36 +0000

    cerrect typo quanity > quantity in bag app views.py & contexts.py and in products app product_details.html

--- bag/views.py
+++ bag/views.py
@@ -10,14 +10,14 @@ def view_bag(request):
 def add_item(request, product_id):
     """ Add a certain number to the shopping bag """
 
-    quanity = int(request.POST.get('quanity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if product_id in list(bag.keys()):
-        bag[product_id] += quanity
+        bag[product_id] += quantity
     else:
-        bag[product_id] = quanity
+        bag[product_id] = quantity
 
     request.session['bag'] = bag
     print(request.session['bag'])
