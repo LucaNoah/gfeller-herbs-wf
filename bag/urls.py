ea2cf429b91134192845d41a91a9f2acfc62c392
LucaNoah <luca.gfeller2@gmail.com>
2023-02-06 19:01:07 +0000

    in the bag app add view edit_item & wire up urls to adjust the bag item in directly in the bag, in shopping_bah.html remove JavaScrip to delete item from bag as not needed

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='shopping_bag'),
     path('add-item/<product_id>', views.add_item, name='add_item'),
+    path('edit-item/<product_id>/', views.edit_item, name='edit_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -8,7 +8,7 @@ def view_bag(request):
     return render(request, 'bag/shopping_bag.html')
 
 def add_item(request, product_id):
-    """ Add a certain number to the shopping bag """
+    """ Add a certain number of an item to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -33,3 +33,29 @@ def add_item(request, product_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def edit_item(request, product_id):
+    """ A view to change the number of an item or to remove it """
+
+    quantity = int(request.POST.get('quantity'))
+    weight = None
+    if 'product_weight' in request.POST:
+        weight = request.POST['product_weight']
+    bag = request.session.get('bag', {})
+
+    if weight:
+        if quantity > 0:
+            bag[product_id]['items_by_weight'][weight] = quantity
+        else:
+            del bag[product_id]['items_by_weight'][weight]
+            if not bag[product_id]['items_by_weight']:
+                bag.pop(product_id)
+    else:
+        if quantity > 0:
+            bag[product_id] = quantity
+        else:
+            bag.pop(product_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('shopping_bag'))
