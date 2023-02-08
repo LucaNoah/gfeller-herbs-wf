0a2f539758564a7df26222435e2a1160454daf92
LucaNoah <luca.gfeller2@gmail.com>
2023-02-08 16:03:29 +0000

    in bag app in add_item and edit_item view call the toast messages when add or update item inside shopping bag

--- bag/views.py
+++ bag/views.py
@@ -25,14 +25,17 @@ def add_item(request, product_id):
         if product_id in list(bag.keys()):
             if weight in bag[product_id]['items_by_weight'].keys():
                 bag[product_id]['items_by_weight'][weight] += quantity
+                messages.success(request, f'{weight} of {product.name} was successfully updated to {bag[product_id]["items_by_weight"][weight]} in your shopping bag!')
             else:
                 bag[product_id]['items_by_weight'][weight] = quantity
+                messages.success(request, f'{weight} of {product.name} was successfully added to your shopping bag!')
         else:
             bag[product_id] = {'items_by_weight': {weight: quantity}}
+            messages.success(request, f'{weight} of {product.name} was successfully added to your shopping bag!')
     else:
         if product_id in list(bag.keys()):
             bag[product_id] += quantity
-            messages.success(request, f'{product.name} was successfully added to your shopping bag!')
+            messages.success(request, f'{product.name} was successfully updated to {bag[product_id]["items_by_weight"][weight]} in your shopping bag!')
         else:
             bag[product_id] = quantity
             messages.success(request, f'{product.name} was successfully added to your shopping bag!')
@@ -44,6 +47,7 @@ def add_item(request, product_id):
 def edit_item(request, product_id):
     """ A view to change the number of an item or to remove it """
 
+    product = Product.objects.get(pk=product_id)
     quantity = int(request.POST.get('quantity'))
     weight = None
     if 'product_weight' in request.POST:
@@ -53,15 +57,19 @@ def edit_item(request, product_id):
     if weight:
         if quantity > 0:
             bag[product_id]['items_by_weight'][weight] = quantity
+            messages.success(request, f'{weight} of {product.name} was successfully updated to {bag[product_id]["items_by_weight"][weight]} in your shopping bag!')
         else:
             del bag[product_id]['items_by_weight'][weight]
             if not bag[product_id]['items_by_weight']:
                 bag.pop(product_id)
+                messages.success(request, f'{weight} of {product.name} was successfully removed from your shopping bag!')
     else:
         if quantity > 0:
             bag[product_id] = quantity
+            messages.success(request, f'{product.name} was successfully updated to {bag[product_id]["items_by_weight"][weight]} in your shopping bag!')
         else:
             bag.pop(product_id)
+            messages.success(request, f'{product.name} was successfully removed from your shopping bag!')
 
     request.session['bag'] = bag
     return redirect(reverse('shopping_bag'))
