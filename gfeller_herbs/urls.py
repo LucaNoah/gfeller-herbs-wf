b05ed22cd8fe70c59d580bf12b16397a29922955
LucaNoah <luca.gfeller2@gmail.com>
2023-02-15 16:02:59 +0000

    in checkout.html add checkout form and link checkout template on shopping bag

--- gfeller_herbs/urls.py
+++ gfeller_herbs/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    path('chekcout/', include('checkout.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
