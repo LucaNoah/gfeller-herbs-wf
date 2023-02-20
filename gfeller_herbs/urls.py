1108f89d4b73cb95e8f584a982d204c51d0f29b1
LucaNoah <luca.gfeller2@gmail.com>
2023-02-20 18:14:09 +0000

    add media url to project level urls

--- gfeller_herbs/urls.py
+++ gfeller_herbs/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('account/', include('accounts.urls')),
-]
+] + static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
