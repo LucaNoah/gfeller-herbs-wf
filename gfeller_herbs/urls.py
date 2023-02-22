94b578a4444353ce1ec4a020d9628e8dc3c8f2b6
LucaNoah <luca.gfeller2@gmail.com>
2023-02-22 15:52:30 +0000

    add images to some products via admin panel and implement images in products and products_detail template

--- gfeller_herbs/urls.py
+++ gfeller_herbs/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('account/', include('accounts.urls')),
-] + static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
