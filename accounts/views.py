3eb887a7d869d700699ad7da1d8e7d0ff47d2a2a
LucaNoah <luca.gfeller2@gmail.com>
2023-02-25 15:45:37 +0000

    products app views fix import of user passes test

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth import login_required
+from django.contrib.auth.decorators import login_required
 
 from .models import UserAccount
 from .forms import UserAccountForm
