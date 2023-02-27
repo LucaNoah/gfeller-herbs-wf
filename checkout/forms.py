2e2666338b38cac4669be5d64222e7191fc11f3c
LucaNoah <luca.gfeller2@gmail.com>
2023-02-27 16:45:23 +0000

    add js script for stripe to base.html, in checkout apps create static & js folder and stripe_elements.js setup stripe inside & add script to template, inside static folder create checkout.css and add style for stripe, in forms.py create method to add classes for styling to input fields in checkout.html template add postloadjs block and add stripe public key & client secret variables inside add these variables to checkout views context

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,3 +15,11 @@ class OrderForm(forms.ModelForm):
             "state",
             "country",
         )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add classes to all fields
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
