6027944071324cf6890079f67a9eeed3fb036679
LucaNoah <luca.gfeller2@gmail.com>
2023-03-02 19:43:09 +0000

    in settings.py get SECRET KEY from environ

--- gfeller_herbs/settings.py
+++ gfeller_herbs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!n)@$edy=tn5ljkdcuh8+m@v#lqo@=qt@-lx=g2qo#+xtk(l@a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
