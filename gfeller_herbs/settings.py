ded64a4752e4125ebbe1caa27a4303d109184b61
LucaNoah <luca.gfeller2@gmail.com>
2023-08-31 14:57:06 +0200

    in settings.py edit heroku url from allowed hosts

--- gfeller_herbs/settings.py
+++ gfeller_herbs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gfeller-herbs-bs.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["gfeller-herbs-bs-34fbbcc26d47.herokuapp.com", "localhost"]
 
 # Application definition
 
