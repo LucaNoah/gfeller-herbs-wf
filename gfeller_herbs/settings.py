6ec115440a7f871035e747961d83a9de64d2ff50
LucaNoah <luca.gfeller2@gmail.com>
2023-08-31 15:04:36 +0200

    in setting.py set debug to True if DEVELOPMENT is in environ

--- gfeller_herbs/settings.py
+++ gfeller_herbs/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["gfeller-herbs-bs-34fbbcc26d47.herokuapp.com", "localhost"]
 
